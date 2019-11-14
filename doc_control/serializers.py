693b8dbeddcac95352ae797bfd21f6ee90b65f75
mElbo <mortenelbo01@gmail.com>
2019-11-14 17:26:04 +0000

    small backend working

---
+++ doc_control/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import Document, Update
+
+class NestedUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Update
+        fields = ('id', 'updated_by', 'update_decription', 'updated_at', 'document')
+
+class NestedDocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = ('id', 'decription', 'created_at')
+
+class UpdateSerializer(serializers.ModelSerializer):
+
+    document = NestedDocumentSerializer()
+
+    class Meta:
+        model = Update
+        fields = ('id', 'updated_by', 'updated_at', 'update_decription', 'document')
+
+class DocumentSerializer(serializers.ModelSerializer):
+    
+    updates = NestedUpdateSerializer(required=False) 
+    class Meta:
+        model = Document
+        fields = ('id', 'decription', 'created_at', 'updates', 'document')
--- doc_control/views.py
+++ doc_control/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+# pylint: disable=no-member
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Document, Update
+from .serializers import DocumentSerializer, UpdateSerializer
 
 # Create your views here.
+
+class DocumentListView(ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
+class DocumentDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
+class UpdateListView(ListAPIView):
+    queryset = Update.objects.all()
+    serializer_class = UpdateSerializer
+
+class UpdateDetailView(RetrieveAPIView):
+    queryset = Update.objects.all()
+    serializer_class = UpdateSerializer
+
+
+# class Document_numberListView(ListAPIView):
+#     queryset = Document_number.objects.all()
+#     serializer_class = Document_numberSerializer
+
+
+# class Document_numberDetailView(RetrieveAPIView):
+#     queryset = Document_number.objects.all()
+#     serializer_class = Document_numberSerializer
