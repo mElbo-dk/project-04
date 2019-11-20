f6e53069a9c3422e610da71e462c2380fee29ab9
Olly Aulakh <oaulakh@streamamp.com>
2019-11-20 12:50:43 +0000

    update styling4

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -5,9 +5,11 @@ class Home extends React.Component {
   render() {
     return (
     <>
+    <hr></hr>
       <h1>Welcome to Document Version Control</h1>
 
       <img className='homeImage' src='../../assets/cabinet.jpg' alt='file cabinet' height='300px'/>
+      <hr></hr>
       </>
     )
   }
