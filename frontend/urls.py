fcab08ebc34832ed525dce7a37c17f049c604328
mElbo <mortenelbo01@gmail.com>
2019-11-21 17:17:25 +0000

    deployment

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, re_path
+from django.urls import re_path
 from .views import Home, Assets
 urlpatterns = [
- path('', Home.as_view()),
- re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view())
+  re_path(r'^(?!/api)[\w\?\=\-\/]*$', Home.as_view()),
+  re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view())
 ]
